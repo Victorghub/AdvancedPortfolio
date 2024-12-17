e08032d07fec67ba26d62fbcedae90de7dd8b7a2
Victor <onetworacks@gmail.com>
2024-12-17 14:39:33 +0000

    JS

---
+++ index.js
@@ -0,0 +1,18 @@
+// template_qjo9svd
+// service_ylb3grx
+// oXAceCEXZ2PXuo1ck
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible");
+        console.log('it worked')
+    }, 1000);
+}
+
+contact()
+
