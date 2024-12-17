f998e4ff3514970de5d021e21680e4d9ae2ea175
Victor <onetworacks@gmail.com>
2024-12-17 15:33:25 +0000

    js

--- index.js
+++ index.js
@@ -5,14 +5,26 @@
 
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_ylb3grx",
+        "template_qjo9svd",
+        event.target,
+        "oXAceCEXZ2PXuo1ck"
+      )
+      .then(() => {
         loading.classList.remove("modal__overlay--visible");
-        console.log('it worked')
-    }, 1000);
-}
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+  }
 
-contact()
 
