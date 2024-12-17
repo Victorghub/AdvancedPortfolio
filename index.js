fb093fc79aff017f913816c0d694cef4aa43c5c0
Victor <onetworacks@gmail.com>
2024-12-17 17:51:49 +0000

    Fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function contact(event) {
       .catch(() => {
         loading.classList.remove("modal__overlay--visible");
         alert(
-          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+          "The email service is temporarily unavailable. Please contact me directly on itsvictorlopez@gmail.com"
         );
       });
   }
