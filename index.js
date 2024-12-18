40ed9c9b0f574edb131bc236d71af4705efd6758
Victor <onetworacks@gmail.com>
2024-12-18 17:16:53 +0000

    finished portfolio just need to add some other touches like CV and maybe change the color of some things

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// template_qjo9svd
-// service_ylb3grx
-// oXAceCEXZ2PXuo1ck
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 
 function contact(event) {
@@ -11,7 +11,7 @@ function contact(event) {
     emailjs
       .sendForm(
         "service_ylb3grx",
-        "template_qjo9svd",
+        "template_1cw7zvk",
         event.target,
         "oXAceCEXZ2PXuo1ck"
       )
@@ -28,3 +28,36 @@ function contact(event) {
   }
 
 
+
+  function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+  }
+
+ function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme");
+  }
+  }
+
+  function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+  }
+  
+  
